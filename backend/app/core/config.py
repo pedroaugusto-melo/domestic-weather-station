6f3224e0e79951735e2bef60a7b833baa91da7b3
Pedro Augusto Melo <pedro@startcarreiras.com>
2024-11-24 16:43:00 -0300

    fix: dark mode as default theme

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -35,6 +35,7 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     FRONTEND_HOST: str = "http://localhost:5173"
+    FRONTEND_ALT_HOST: str = "http://127.0.0.1:5173"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
     BACKEND_CORS_ORIGINS: Annotated[
@@ -45,7 +46,7 @@ class Settings(BaseSettings):
     @property
     def all_cors_origins(self) -> list[str]:
         return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
-            self.FRONTEND_HOST
+            self.FRONTEND_HOST, self.FRONTEND_ALT_HOST
         ]
 
     PROJECT_NAME: str
